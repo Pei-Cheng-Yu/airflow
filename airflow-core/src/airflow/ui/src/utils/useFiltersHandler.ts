7dfc3252b9c070ef2a77f0abec756a5cfb4fb21b
seanyu <seansean988@gmail.com>
2025-11-12 16:06:56 +0800

    fix format

--- airflow-core/src/airflow/ui/src/utils/useFiltersHandler.ts
+++ airflow-core/src/airflow/ui/src/utils/useFiltersHandler.ts
@@ -44,9 +44,9 @@ export type FilterableSearchParamsKeys =
   | SearchParamsKeys.LOGICAL_DATE_LTE
   | SearchParamsKeys.MAP_INDEX
   | SearchParamsKeys.NAME_PATTERN
+  | SearchParamsKeys.OPERATOR_NAME_PATTERN
   | SearchParamsKeys.POOL_NAME_PATTERN
   | SearchParamsKeys.QUEUE_NAME_PATTERN
-  | SearchParamsKeys.OPERATOR_NAME_PATTERN
   | SearchParamsKeys.RESPONDED_BY_USER_NAME
   | SearchParamsKeys.RESPONSE_RECEIVED
   | SearchParamsKeys.RUN_AFTER_GTE
